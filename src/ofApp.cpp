4b0660b9fb8f4af522c80481d5befa1a153ea592
Christoph Franke <cf1981@gmx.de>
2015-08-17 18:38:34 +0200

    comment

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -6,15 +6,16 @@ void ofApp::setup(){
 	ofSetFrameRate(30);
 	ofBackground(0);
 
+	
+	soundStream.listDevices();
+
+	//if you want to set a different device id, default is microphone-in i guess (at least it is on all of my PCs)?
+	//soundStream.setDeviceID(1);  
+
 	// 0 output channels, 
 	// 2 input channels
 	// 44100 samples per second
-	// 256 samples per buffer
 	// 4 num buffers (latency)
-	soundStream.listDevices();
-
-	//if you want to set a different device id 
-	//soundStream.setDeviceID(1); //bear in mind the device id corresponds to all audio devices, including  input-only and output-only devices.
 	soundStream.setup(this, 0, 2, 44100, BUFFER_SIZE, 4);
 
 	samples = new float[BUFFER_SIZE * 2];
@@ -35,45 +36,42 @@ void ofApp::draw(){
 
 	ofNoFill();
 
-	// draw the left channel:
 	ofPushStyle();
 	ofPushMatrix();
 	ofTranslate(200, 150, 0);
 
 	ofSetColor(245, 58, 135);
 	ofSetLineWidth(3);
-
+	
+	// draw the left channel:
 	ofBeginShape();
 	for (unsigned int i = 0; i < BUFFER_SIZE / 2; i++){
 		ofVertex(50 + i * 2, 100 - samples[i * 2] * 180.0f);
 	}	
 	ofEndShape(false);
-
+	
+	// draw the right channel:
 	ofBeginShape();
 	for (unsigned int i = 0; i < BUFFER_SIZE / 2; i++){
 		ofVertex(650 + i * 2, 100 - samples[i * 2 + 1] * 180.0f);
 	}	
 	ofEndShape(false);
 
-	/* do the FFT	*/
+	// do the FFT and compute spectrum
 	BmFFT::getSpectrum(MAGNITUDE, samples, N_BANDS, bandVolumes);
 
 
-	/* draw the FFT */
+	// draw the FFT
 	for (int i = 0; i < N_BANDS; i++){
-		ofLine(50 + (i * 3), 600, 50 + (i * 3), 600 - bandVolumes[i * 2] * 300);
-		ofLine(650 + (i * 3), 600, 650 + (i * 3), 600 - bandVolumes[i * 2 + 1] * 300);
+		ofLine(50 + (i * 3), 600, 50 + (i * 3), 600 - bandVolumes[i * 2] * 300); //left
+		ofLine(650 + (i * 3), 600, 650 + (i * 3), 600 - bandVolumes[i * 2 + 1] * 300); //right
 	}
-
-
+	
 	ofPopMatrix();
 	ofPopStyle();
-
-
 }
 
 void ofApp::audioReceived(float * input, int bufferSize, int nChannels){
-	// samples are "interleaved"
 	for (int i = 0; i < bufferSize * 2; i++){
 		samples[i] = input[i];
 	}
